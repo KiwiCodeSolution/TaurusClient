99dd33a5a98b61695ecea3136f43d69b2ad7d527
Evgeniya Poduzova <99011054+Evgenija-P@users.noreply.github.com>
2024-04-04 00:02:03 +0300

    loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,11 +1,12 @@
 import { PropagateLoader } from "react-spinners";
-import Overlay from "./UI/modal/Overlay";
 
 const Loader = () => {
   return (
-    <Overlay>
-      <PropagateLoader color="#ffd698" />
-    </Overlay>
+    <div className="fixed inset-0 w-full h-screen bg-[rgb(0,0,0,0.35)] z-[100] top-0 backdrop-blur">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-100">
+        <PropagateLoader color="#ffd698" />
+      </div>
+    </div>
   );
 };
 
