6a9630da0b79e48353e4233c8e2975725940ae69
Evgeniya Poduzova <99011054+Evgenija-P@users.noreply.github.com>
2025-03-14 13:55:02 +0200

    Update config.js

--- src/API/config.js
+++ src/API/config.js
@@ -1,6 +1,6 @@
 // export const baseServerURL = "http://localhost:5000/";
 
-export const baseServerURL = "http://kiwicode.tech:5000/";
+// export const baseServerURL = "http://kiwicode.tech:5000/";
 // export const baseServerURL = "https://kiwicode.tech:5000/";
 
-// export const baseServerURL = "https://taurusserver.onrender.com/";
+export const baseServerURL = "https://taurusserver.onrender.com/";
